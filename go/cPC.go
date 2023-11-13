23deac4243782dfb4c46a7bba0b14e964b10430e
leonardoCaceres <leolima212@hotmail.com>
2023-11-13 19:57:01 -0300

    adding go

---
+++ go/cPC.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"sync"
+	"strconv"
+)
+
+type Objeto struct {
+	ID    int
+	Total int
+	Grupo int
+}
+
+func NewObjeto(id int) Objeto {
+	return Objeto{
+		ID:    id,
+		Total: rand.Intn(11),
+		Grupo: rand.Intn(5) + 1,
+	}
+}
+
+func main() {
+	N, _ := strconv.Atoi(os.Args[1])
+	T, _ := strconv.Atoi(os.Args[2])
+	base10N := int(math.Pow(10, float64(N)))
+	totais := 0
+	totaisGrupo := [5]int{0}
+	somas := [264]int{0}
+	somasGrupo := make([][]int, 264)
+	listaMenor := make([][]int, 264)
+	listaMaior := make([][]int, 264)
+	
+	for i := range somasGrupo {
+		somasGrupo[i] = make([]int, 5)
+	}
+
+	rand.Seed(27600)
+
+	lista := make([]Objeto, base10N)
+
+	for i := 0; i < base10N; i++ {
+		lista[i] = NewObjeto(i)
+	}
+
+	teto := float64(base10N) / float64(T)
+
+	var wg sync.WaitGroup
+
+	//// Processamento
+	for i := 0; i < T; i++ {
+		wg.Add(1)
+		i := i
+		go func () {
+			defer wg.Done()
+			inicio := int(math.Ceil(teto * float64(i)))
+			fim := int(math.Ceil(teto * float64(i+1)))
+					
+			for j := inicio; j < fim; j++ {
+				somas[i] += lista[j].Total
+				somasGrupo[i][lista[j].Grupo-1] += lista[j].Total
+
+				if lista[j].Total < 5 {
+					listaMenor[i] = append(listaMenor[i], lista[j].ID)
+							
+				} else {
+						listaMaior[i] = append(listaMaior[i], lista[j].ID)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	
+
+	
+	for i := 0; i < T; i++ {
+		totais += somas[i]
+		for j := 0; j < 5; j++ {
+			totaisGrupo[j] += somasGrupo[i][j]
+		}
+	}
+
+	finalMenorList := make([]int, 0)
+	finalMaiorList := make([]int, 0)
+
+	for i := 0; i < T; i++ {
+		finalMenorList = append(finalMenorList, listaMenor[i]...)
+		finalMaiorList = append(finalMaiorList, listaMaior[i]...)
+	}
+	/// Processamento
+	
+	fmt.Println("Total:", totais)
+	fmt.Println("Totais grupo:")
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(totaisGrupo[i])
+	}
+
+	fmt.Println("Quantidade menores que 5:", len(finalMenorList))
+	fmt.Println("Quantidade maiores que 5:", len(finalMaiorList))
+}
+
