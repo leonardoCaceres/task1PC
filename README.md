5082411fc3b67ed1db316b105388a305227ef1f8
leonardoCaceres <leolima212@hotmail.com>
2023-11-13 08:16:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+To run:
+
+g++ cPC.cpp -o teste -fopenmp
+
+./test N_POW T_THREADS
---
+++ cPC.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <omp.h>
+#include <cmath>
+#include <list>
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+class Objeto {
+	public:
+		int ID;
+		int Total;
+		int Grupo;
+		Objeto(int id){
+			ID = id;
+			Total = rand() % 11;
+			Grupo = rand() % 5 + 1;
+		}
+		Objeto(){
+			//Do nothing;
+		}
+};
+
+int main(int argc, char *argv[])
+{
+	int N = atoi(argv[1]), T = atoi(argv[2]), base10N = 0;
+	long int totais = 0, totaisGrupo[5] = {0}, somas[264] = {0}, somasGrupo[264][5] = {0};
+	
+	srand(27600);
+	
+	base10N = pow(10, N);
+	
+	Objeto *lista = new Objeto[base10N];
+	list<int> *listaMenor = new list<int>[T];
+	list<int> *listaMaior = new list<int>[T];
+	list <int> finalMenorList;
+	list <int> finalMaiorList;
+	
+	for(int i=0; i<base10N; i++)
+	{
+		lista[i] = Objeto(i);
+	}
+	
+	float teto = float(base10N)/float(T);
+	
+	////Processamento
+	#pragma omp parallel num_threads(T)
+	{
+		int inicio = ceil( teto * omp_get_thread_num() );
+		int fim = ceil( teto * (omp_get_thread_num()+1) );
+		for(int i=inicio; i < fim; i++)
+		{
+			
+			somas[omp_get_thread_num()] += lista[i].Total;
+			somasGrupo[omp_get_thread_num()][lista[i].Grupo-1] += lista[i].Total;
+			if(lista[i].Total < 5)
+			{
+				listaMenor[omp_get_thread_num()].push_back(lista[i].ID);
+			}
+			else
+			{
+				listaMaior[omp_get_thread_num()].push_back(lista[i].ID);
+			}
+		}
+	}
+	#pragma omp parallel for reduction(+:totaisGrupo, totais) num_threads(T)	
+		for(int i=0; i<T; i++)
+		{
+			
+			totais += somas[i];
+			for(int j=0; j<5; j++)
+			{
+				totaisGrupo[j] += somasGrupo[i][j];
+			}
+		}
+		
+	for(int i=0; i<T; i++)
+	{
+		finalMenorList.splice(finalMenorList.end(), listaMenor[i]);
+		finalMaiorList.splice(finalMaiorList.end(), listaMaior[i]);
+	}
+	////Processamento
+	cout << "Total:" << totais << endl;
+	cout << "totais grupo: " << endl;
+	
+	for(int i=0; i<5; i++)
+	{
+		cout << totaisGrupo[i] << endl;
+	}
+	
+	cout << "Quantidade menores que 5: " << finalMenorList.size() << endl;
+	cout << "Quantidade maiores que 5: " << finalMaiorList.size() << endl;
+	
+	free (lista);
+}
